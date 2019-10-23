e26d1a8a197581acf94a9f103b592554e4d08121
thi131190 <thi131190@gmail.com>
2019-10-23 17:14:43 +0700

    Implement login by facebook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import FacebookLogin from 'react-facebook-login'
 
 import './App.css'
 import Game from './components/Game'
 
 function App () {
+  const [currentUser, setCurrentUser] = useState(null)
+
+  const responseFacebook = response => {
+    setCurrentUser({
+      name: response.name,
+      email: response.email
+    })
+  }
+
   return (
     <div className='App'>
-      <Game />
+      {!currentUser
+        ? <FacebookLogin
+          autoLoad
+          appId='919737441746550'
+          fields='name,email,picture'
+          callback={responseFacebook}
+          />
+        : <Game currentUser={currentUser} />}
     </div>
   )
 }
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,68 +1,6 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Board from './Board'
-
-const Game = props => {
-  const initialHistory = [{ squares: Array(9).fill(null) }]
-  const [history, setHistory] = useState(initialHistory)
-  const [xIsNext, setXIsNext] = useState(true)
-  const [stepNumber, setStepNumber] = useState(0)
-
-  const handleClick = i => {
-    const slicedHistory = history.slice(0, stepNumber + 1)
-    const finalStepInSlicedHistory = slicedHistory[slicedHistory.length - 1]
-    const newSquares = [...finalStepInSlicedHistory.squares]
-
-    const winnerDeclared = Boolean(calculateWinner(newSquares))
-    const squareAlreadyFilled = Boolean(newSquares[i])
-    if (winnerDeclared || squareAlreadyFilled) return
-
-    newSquares[i] = xIsNext ? 'X' : 'O'
-    const newStep = { squares: newSquares }
-    const newHistory = [...slicedHistory, newStep]
-
-    setHistory(newHistory)
-    setXIsNext(!xIsNext)
-    setStepNumber(slicedHistory.length)
-  }
-  const jumpTo = step => {
-    setStepNumber(step)
-
-    const isEvenStepNumber = step % 2 === 0
-    setXIsNext(isEvenStepNumber)
-  }
-  const moves = history.map((step, move) => {
-    const description = move ? `Go to move #${move}` : `Go to game start`
-    return (
-      <li>
-        <button onClick={() => jumpTo(move)}>
-          {description}
-        </button>
-      </li>
-    )
-  })
-
-  const currentStep = history[stepNumber]
-  const winner = calculateWinner(currentStep.squares)
-  const status = winner
-    ? `Winner: ${winner}`
-    : `Next player: ${xIsNext ? 'X' : 'O'}`
-
-  return (
-    <div className='game'>
-      <div className='game-board'>
-        <Board squares={currentStep.squares} onClick={i => handleClick(i)} />
-      </div>
-      <div className='game-info'>
-        <div>
-          {status}
-        </div>
-        <ol>
-          {moves}
-        </ol>
-      </div>
-    </div>
-  )
-}
+import axios from 'axios'
 
 /**
  * calculateWinner (helper function)
@@ -99,6 +37,118 @@ function calculateWinner (squares) {
   // If none of the winning line combinations is contained in
   // input squares array, return null...
   return null
+}
+
+const Game = props => {
+  const initialHistory = [{ squares: Array(9).fill(null) }]
+  const [history, setHistory] = useState(initialHistory)
+  const [xIsNext, setXIsNext] = useState(true)
+  const [stepNumber, setStepNumber] = useState(0)
+  const [highScores, setHighScores] = useState([])
+
+  const getHighScores = async () => {
+    const res = await axios.get(
+      `https://ftw-highscores.herokuapp.com/tictactoe-dev`
+    )
+    const data = await res.data.items
+    setHighScores(data)
+  }
+
+  const postScore = async () => {
+    console.log('Posting...')
+    const currentDate = new Date()
+    let data = new URLSearchParams()
+    data.append('player', props.currentUser)
+    data.append('score', currentDate.getTime())
+    const url = `https://ftw-highscores.herokuapp.com/tictactoe-dev`
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: data.toString(),
+      json: true
+    })
+    console.log(response)
+  }
+
+  useEffect(() => {
+    getHighScores()
+  }, [])
+
+  const handleClick = i => {
+    const slicedHistory = history.slice(0, stepNumber + 1)
+    const finalStepInSlicedHistory = slicedHistory[slicedHistory.length - 1]
+    const newSquares = [...finalStepInSlicedHistory.squares]
+
+    const winnerDeclared = Boolean(calculateWinner(newSquares))
+    const squareAlreadyFilled = Boolean(newSquares[i])
+    if (winnerDeclared || squareAlreadyFilled) {
+      return
+    }
+    newSquares[i] = xIsNext ? 'X' : 'O'
+    const newStep = { squares: newSquares }
+    const newHistory = [...slicedHistory, newStep]
+
+    setHistory(newHistory)
+    setXIsNext(!xIsNext)
+    setStepNumber(slicedHistory.length)
+  }
+  const jumpTo = step => {
+    setStepNumber(step)
+
+    const isEvenStepNumber = step % 2 === 0
+    setXIsNext(isEvenStepNumber)
+  }
+  const moves = history.map((step, move) => {
+    const description = move ? `Go to move #${move}` : `Go to game start`
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>
+          {description}
+        </button>
+      </li>
+    )
+  })
+
+  const currentStep = history[stepNumber]
+
+  const winner = calculateWinner(currentStep.squares)
+
+  const status = winner
+    ? `Winner: ${winner}`
+    : `Next player: ${xIsNext ? 'X' : 'O'}`
+
+  winner && postScore()
+
+  return (
+    <div className='game'>
+      <div className='game-board'>
+        <Board squares={currentStep.squares} onClick={i => handleClick(i)} />
+      </div>
+      <div className='game-info'>
+        <div>
+          {status}
+        </div>
+        <ol>
+          {moves}
+        </ol>
+      </div>
+      <div className='high-scores'>
+        <div>Top players</div>
+        <ol>
+          {highScores &&
+            highScores.map(player => {
+              return (
+                <li key={player._id}>
+                  {player.player}: {player.score}
+                </li>
+              )
+            })}
+        </ol>
+      </div>
+    </div>
+  )
 }
 
 export default Game
